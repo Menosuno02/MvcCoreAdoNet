7d64aa4f5b511fdd8bcdd6aa7ccdd2deb5f61c6b
Alejandro López Labajo <mainusone1@gmail.com>
2024-02-07 14:23:16 +0100

    Commit

---
+++ MvcCoreAdoNet/Repositories/RepositoryHospital.cs
@@ -0,0 +1,44 @@
+﻿using MvcCoreAdoNet.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MvcCoreAdoNet.Repositories
+{
+    public class RepositoryHospital
+    {
+        SqlConnection cn;
+        SqlCommand com;
+        SqlDataReader reader;
+
+        public RepositoryHospital()
+        {
+            string connectionString = @"Data Source=LOCALHOST\SQLEXPRESS;Initial Catalog=HOSPITAL;User ID=sa;Password=MCSD2023;Encrypt=False";
+            this.cn = new SqlConnection(connectionString);
+            this.com = new SqlCommand();
+            this.com.Connection = this.cn;
+        }
+
+        public List<Hospital> GetHospitales()
+        {
+            string sql = "SELECT * FROM HOSPITAL";
+            this.com.CommandText = sql;
+            this.com.CommandType = CommandType.Text;
+            this.cn.Open();
+            this.reader = this.com.ExecuteReader();
+            List<Hospital> hospitales = new List<Hospital>();
+            while(this.reader.Read())
+            {
+                Hospital hospital = new Hospital();
+                hospital.IdHospital = int.Parse(this.reader["HOSPITAL_COD"].ToString());
+                hospital.Nombre = this.reader["NOMBRE"].ToString();
+                hospital.Direccion = this.reader["DIRECCION"].ToString();
+                hospital.Telefono = this.reader["TELEFONO"].ToString();
+                hospital.Camas = int.Parse(this.reader["NUM_CAMA"].ToString());
+                hospitales.Add(hospital);
+            }
+            this.reader.Close();
+            this.cn.Close();
+            return hospitales;
+        }
+    }
+}
